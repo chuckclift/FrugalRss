6d57f3d52d47ef28e537c927af4984259b3c2c4f
chuckclift <cclift119@gmail.com>
2015-06-06 20:40:04 -0400

    Changed try/catch block in main loop to if else

--- frugalRss.py
+++ frugalRss.py
@@ -24,6 +24,7 @@ for html in siteHtml:
     items = html.split("<item>")
     items = items[1:-1]  ##removing the first element (where ther are no items)
     siteData = []
+
     for it in items:
         title = it.split("<title>")[1].split("</title>")[0]
         link = it.split("<link>")[1].split("</link>")[0]
@@ -36,17 +37,17 @@ sitesRemoved = 0
 ##getting the article html documents
 ##the urls are shuffled so that the program is
 ##not accessing any one server consecutively
-##except at the end (when 
+##except at the end 
 
 
-middleData = []
+htmlData = []
 while metaData:
-    for site in metaData: 
-        try:
+    for site in metaData:
+        if len(site) > 0:
             workingData = site.pop()
             html = requests.get(workingData[1])
             html = html.text
-            middleData.append((workingData[0],html))
+            htmlData.append((workingData[0],html))
             
             print(workingData[0])
 
@@ -55,21 +56,20 @@ while metaData:
             if len(metaData) > 2:
                 time.sleep(3)
                 
-        except IndexError:
+        else:
             metaData.remove(site)
-            sitesRemoved += 1 
-            print("site number " , sitesRemoved , " removed")
 
 
+##extracting the text from the html in htmlData
 finalData = []
-for i in middleData:
+for i in htmlData:
     article = ( "<title>" + i[0] + "</title>" + 
               "<article>" + htmlParser.getParagraphs(i[1]) + "</articles>")
     finalData.append((i[0],article)) 
 
+##writing the results to the text files
 headlines = open("headlines.txt","w", encoding="utf-8")
 articles = open("articles.txt","w", encoding="utf-8")
-
 
 for i in finalData:
     headlines.write(i[0] + "\n")
